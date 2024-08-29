8c781ec489d1e10e148e44f0057666a98288300e
usman972 <usman083922@gmail.com>
2024-08-29 15:24:29 +0500

    update header search form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -41,8 +41,11 @@ const Contact = ({ listing }) => {
             placeholder="Enter your message here.."
             className="w-full border p-3 rounded-lg"
           ></textarea>
-          <Link className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95" to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}>
-          Send Message
+          <Link
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          >
+            Send Message
           </Link>
         </div>
       )}
